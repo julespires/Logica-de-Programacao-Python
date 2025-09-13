54bccc5213e4cd315f59445e3b1684c0af696a58
Jules Pires <julespires@hotmail.com>
2025-09-13 14:27:30 -0300

    Preparo dos exercícios

--- 1-Estrutura Sequencial/Ex-015.py
+++ 1-Estrutura Sequencial/Ex-015.py
@@ -9,8 +9,23 @@ Ex:015
 Data:25/08/2025
 '''
 
+# Biblioteca
+import os
 
+# Limpa a tela do prompt
+os.system('cls')
+
+# Recebe os dados do automóvel
 precoFabrica = float(input("Informe o preço de fábrica:"))
 percLucroDistribuidor = float(input("Informe o percentual de lucro do distribuidor:"))
 percImpostos = float(input("Informe o percentual dos impostos:"))
 
+# Realiza o cálculo
+LucroDistribuidor = precoFabrica * percLucroDistribuidor / 100
+valorImposto = precoFabrica * percImpostos / 100
+precoFinal = precoFabrica + percLucroDistribuidor + percImpostos
+
+# Mostra o resultado
+print("Lucro do distribuídor R$:{}".format(LucroDistribuidor))
+print("Impostos R$:{}".format(valorImposto))
+print("Preço final R$:{}".format(precoFinal))
---
+++ 1-Estrutura Sequencial/Ex-016.py
@@ -0,0 +1,11 @@
+'''
+Faça um programa que receba o número de horas trabalhadas e o valor do salário mínimo, calcule e
+mostre o salário a receber, seguindo estas regras:
+a) a hora trabalhada vale a metade do salário mínimo.
+b) o salário bruto equivale ao número de horas trabalhadas multiplicado pelo valor da hora trabalhada.
+c) o imposto equivale a 3% do salário bruto.
+d) o salário a receber equivale ao salário bruto menos o imposto.
+Autor:Jules do Nascimento Pires
+Ex:016
+Data:12/09/2025
+'''
